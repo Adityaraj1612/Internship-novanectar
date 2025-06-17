669a721a49950dfd6e5ee7dc753506c3cb4e438b
Adityaraj1612 <adityaraj10b@gmail.com>
2025-06-17 17:59:53 +0530

    Remove unnecessary whitespace in Home component for cleaner code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,11 +18,9 @@ export default function Home() {
     <>
       <Hero />
              {/* small three‑column info under hero */}
-<<<<<<< HEAD
+
       <FeaturedCourses />
-=======
-              <FeaturedCourses />
->>>>>>> 2238abaa5f1647dc57629e23faa7ed8850ec15e3
+       
       <TopPicks />
       <Community />
       <StatsBar />
